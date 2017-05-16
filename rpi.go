139205f3379ae28c2177fc6241da81e4843d64be
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2017-05-16 09:58:57 +0300

    message pulling from I2C depending on interrupt pin, both go and python

--- rpi.go
+++ rpi.go
@@ -3,22 +3,31 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/stianeikeland/go-rpio"
 	"github.com/yaman/i2c"
 )
 
 func main() {
+	err := rpio.Open()
 
-		i2cbus := i2c.NewI2CBus(1)
+	if nil != err {
+		fmt.Println("error occured :", err)
+	}
+	pin := rpio.Pin(10)
+
+	i2cbus := i2c.NewI2CBus(1)
 	for {
-		text, err := i2cbus.ReadBytes(0x60,400)
+		for pin.Read() == rpio.High {
+			text, err := i2cbus.ReadBytes(0x60, 400)
 
-		if err != nil {
-			fmt.Println("iinit errorr: ", err)
+			if err != nil {
+				fmt.Println("iinit errorr: ", err)
+			}
+
+			fmt.Println("how  many: ", text[0])
+			fmt.Println("text: ", text[0:])
+			time.Sleep(100 * time.Millisecond)
 		}
-
-		fmt.Println("how many: ", text[0])
-		fmt.Println("text: ", text[0:])
-		time.Sleep(100 * time.Millisecond)
 	}
 }
-
